4bd5629942ec457fffa01c7b8876d81c37323aa3
inincevic <inincevic@unipu.hr>
2022-04-01 09:25:53 +0200

    Added everything needed for displaying required

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="app">
+    <ul id="example-1" v-if="book.name">
+        <li>{{book.name}}</li>
+    </ul>
+  </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Details",
+  props: ["name"],
+
+  data() {
+    return { book: {} };
+  },
+  methods: {
+    async getBookData() {
+      try {
+        const response = await axios.get(
+          "http://cantab.elaclo.com:8000/books.json"
+        );
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getBookData().then((data) => {
+      this.book = data.find(
+        (book) => book.name === localStorage.getItem("requestedName")
+      );
+      console.log(this.commit);
+    });
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul id="example-1">
+      <li v-for="book in this.simpleBookData" :key="book">
+        Book: <router-link to="/details/name" @click="setBook(book.name)">{{ book.name }}</router-link>
+        <br />
+        First author: {{ book.authors }}
+        <br />
+        Release date: {{ book.released }}
+
+        <br />
+        <br />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  data() {
+    return { simpleBookData: [] };
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(
+          "http://cantab.elaclo.com:8000/books.json"
+        );
+        this.simpleBookData = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    consolePrintData() {
+      console.log(this.simpleBookData);
+    },
+    setBook(name) {
+      localStorage.setItem("requestedBook", name);
+    },
+  },
+
+  created() {
+    this.getData();
+    this.consolePrintData();
+  },
+};
 </script>
